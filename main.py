54515e472f3527dd935a927d3afa7400914bfee3
david <david.pergament@student.kyh.se>
2020-08-31 13:59:25 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Jag heter David Pergament, född och uppvuxen i Göteborg")
+print ("Jag har aldrig programmerat förut")
+print ("Jag sökte till Pvt för att IT är en stabil bransch och för att det alltid kommer finnas behov för testare")
