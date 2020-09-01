56c3570c1f9b14b1e5fbc901297beb491c73d442
david <david.pergament@student.kyh.se>
2020-09-01 09:11:25 +0200

    delete delete

--- main.py
+++
@@ -1,3 +0,0 @@
-print("Jag heter David Pergament, född och uppvuxen i Göteborg")
-print ("Jag har aldrig programmerat förut")
-print ("Jag sökte till Pvt för att IT är en stabil bransch och för att det alltid kommer finnas behov för testare")
