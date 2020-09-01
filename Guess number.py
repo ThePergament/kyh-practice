e145f18aefe234ceb65732cbe40c2b00787b9758
david <david.pergament@student.kyh.se>
2020-09-01 11:23:57 +0200

    enkelt nummerprogram  + def mainloop

--- Guess number.py
+++ Guess number.py
@@ -3,19 +3,26 @@ import random
 n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1-100, gissa vilket?")
 guesses =1
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
-
-    if as_number == n:
-        print("Bra jobbat! Det tog" +str (guesses)+"gissningar!")
-        break
 
 
-    if as_number < n:
-        print("Fel! Mitt  nummer är högre...försök igen!")
-        guesses = guesses + 1
+def mainloop(guesses):
+    n = random.randint(1, 100)
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
 
-    if as_number > n:
-        print("Fel! Mitt nummer är lägre...Försök igen!")
-        guesses = guesses + 1
+        if as_number == n:
+            print("Bra jobbat! Det tog" +str (guesses)+"gissningar!")
+
+            break
+
+
+        if as_number < n:
+            print("Fel! Mitt  nummer är högre...försök igen!")
+            guesses = guesses + 1
+
+        if as_number > n:
+            print("Fel! Mitt nummer är lägre...Försök igen!")
+            guesses = guesses + 1
+
+mainloop(guesses)
