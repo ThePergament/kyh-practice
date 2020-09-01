4672fcfae8dd83631e2c62a9ef22e1ca90d1e866
david <david.pergament@student.kyh.se>
2020-09-01 10:40:30 +0200

    enkelt nummerprogram3

--- Guess number.py
+++ Guess number.py
@@ -2,17 +2,20 @@ import random
 
 n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1-100, gissa vilket?")
-
+guesses =1
 while True:
     text = input("Din gissning: ")
     as_number = int(text)
 
     if as_number == n:
-        print("Rätt!")
+        print("Bra jobbat! Det tog" +str (guesses)+"gissningar!")
         break
+
 
     if as_number < n:
         print("Fel! Mitt  nummer är högre...försök igen!")
+        guesses = guesses + 1
 
     if as_number > n:
         print("Fel! Mitt nummer är lägre...Försök igen!")
+        guesses = guesses + 1
