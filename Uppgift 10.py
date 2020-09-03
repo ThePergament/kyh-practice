a79603979a03614f471931c1092130ff20ff6dec
david <david.pergament@student.kyh.se>
2020-09-03 10:34:27 +0200

    Heltal + Udda tal + 10-1

--- Uppgift 10.py
+++ Uppgift 10.py
@@ -1,2 +1,17 @@
-for i in range (1,11):
-    print (i)
+def main ():
+    print ("Part 1:")
+    for i in range (1,11):
+        print (i)
+
+    print ("Part 2:")
+    for i in range (1,50):
+        if i%2 !=0:
+            print (i)
+
+    print ("Part 3:")
+    for i in range (10,0,-1):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
