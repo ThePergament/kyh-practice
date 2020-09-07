bf94479ee831eda526c30ab58614834c4480c559
david <david.pergament@student.kyh.se>
2020-09-07 11:46:57 +0200

    Uppgift 12.1,12.2,12.3,12.4

--- Uppgift 12.py
+++ Uppgift 12.py
@@ -1,11 +1,16 @@
 
-def is_it_too_long(name):
-    return len(name) >
+def is_it_too_long(name, max_length):
+    return len(name) > max_length
 def main():
-        students ="anna beatrice cecilia doris esmeralda frida gunilla".split()
+        #students ="anna beatrice cecilia doris esmeralda frida gunilla".split()
+        try:
+            max_length = int(input("Ange maxlängd"))
+        except ValueError:
+            max_length = 5
         students =input ("Ange studenternas namn med komma emellan: ").split(",")
         for name in students:
-            if is_it_too_long(name):
+            fixed_name = name.strip()
+            if is_it_too_long(fixed_name,max_length):
                 print(f"{name.title()} är för långt!")
 
 
