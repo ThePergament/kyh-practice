c7fc3ceaa9e076b265651b26a1a02f365070723a
david <david.pergament@student.kyh.se>
2020-09-07 17:21:14 +0200

    More f-strings

---
+++ more f-strings.py
@@ -0,0 +1,5 @@
+name = "Jeff"
+age = "34"
+
+print(f"This is a story about {name}.")
+print (f"{name} is {age} years old and has not found his purpose in life. Because of this, he suffers from anxiety")
