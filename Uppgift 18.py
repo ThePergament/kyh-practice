404cabc209b4d6e763608bc8ebd1fb478f0a2260
david <david.pergament@student.kyh.se>
2020-09-10 10:46:33 +0200

    18.1

---
+++ Uppgift 18.py
@@ -0,0 +1,13 @@
+people = {
+'Fredrik': '0702778511',
+'Olof': '123456789',
+'Lisa': '9999999999',
+'Bodil':'555-666-789'
+}
+
+vem = input('Vem vill du ringa?')
+if vem not in people:
+  print("Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog")
+else:
+  number = people[vem]
+  print(f"Numret till {vem} är {number}")
