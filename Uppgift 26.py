143d572fec39408adf4d30a4ef3819a9d04da5fb
david <david.pergament@student.kyh.se>
2020-09-16 11:20:53 +0200

    26.1

---
+++ Uppgift 26.py
@@ -0,0 +1,14 @@
+
+import requests
+import json
+import datetime
+from pprint import pprint
+
+r=requests.get('http://www.omdbapi.com/',params = {"t": "Alien", "apikey": "9f6d550c"})
+
+data=r.json() #loads(r.text)
+pprint (data)
+
+
+
+#?t=Titanic&apikey=9f6d550c
