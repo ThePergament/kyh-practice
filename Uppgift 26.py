278b6ed9d563b28b55c60f896557b8dc1239ea80
david <david.pergament@student.kyh.se>
2020-09-16 16:57:25 +0200

    Uppgift 26.1, 26.2,26.3

--- Uppgift 26.py
+++ Uppgift 26.py
@@ -1,13 +1,30 @@
 
 import requests
 import json
-import datetime
 from pprint import pprint
 
-r=requests.get('http://www.omdbapi.com/',params = {"t": "Alien", "apikey": "9f6d550c"})
+movie=input("Ange film:")
+r=requests.get('http://www.omdbapi.com/',params = {"t": movie, "apikey": "9f6d550c"})
+text=r.text
+result=json.loads(text)
 
-data=r.json() #loads(r.text)
-pprint (data)
+pprint('*** Resultat från OMDB! ***')
+titel=result["Title"]
+year=result["Year"]
+director=result["Director"]
+actors=result["Actors"]
+rating=result ["imdbRating"]
+awards=result["Awards"]
+runtime=result["Runtime"]
+print(f"Titel:{titel} ({year}) regisserades av {director}\n "
+      f"Skådisar: {actors}\n"
+      f"IMDB betyg: {rating}\n"
+      f"Kaka:{awards}\n"
+      f"   Längd:{runtime}")
+
+
+
+
 
 
 
