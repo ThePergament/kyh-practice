8b9a187b6b2d4990cd8ddee6a9d8de7e25c614cd
david <david.pergament@student.kyh.se>
2020-09-21 14:57:19 +0200

    31.1

---
+++ Uppgift 31.py
@@ -0,0 +1,9 @@
+heltal=input("Ange tal med komma emellan:").split(",")
+ls=heltal
+ls2 =[int (elem)for elem in ls]
+
+print(f"Första talet: {heltal[0]}")
+print(f"Andra talet: {heltal[0]}")
+print(f"Summan av av talen är: {sum(ls2)}")
+backwards=", ".join (ls[::-1])
+print(f"Talen baklänges: {backwards}")
