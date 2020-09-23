8b212fd5fff54d0e266c03cc361d309d56004109
david <david.pergament@student.kyh.se>
2020-09-23 11:03:02 +0200

    Uppgift 35.1

---
+++ quizzservice.py
@@ -0,0 +1,24 @@
+import requests
+
+ENDPOINT = 'https://mqif4s7obg.execute-api.eu-central-1.amazonaws.com/olofs_lambda'
+
+
+class QuizzWebServiceAPI(object):
+
+    def __init__(self):
+        self.url = ENDPOINT
+
+    def get_all_questions(self):
+        r = requests.get(self.url)
+        ls = r.json()['questions']
+        return ls
+
+    def add_question(self, prompt, answer, alternatives):
+        data = {
+            'prompt': prompt,
+            'rightAnswer': answer,
+            'wrongAnswers': alternatives
+        }
+        # Uppgift 35.2: fyll i ???
+        r = requests.???(url=self.url, ???)
+        return True if r.status_code == 200 else False
