516a6c360d10b500d47bcbcbb9281435f010c0b4
david <david.pergament@student.kyh.se>
2020-09-28 14:46:14 +0200

    40.1, 40.2

--- Uppgift 40.py
+++ Uppgift 40.py
@@ -1,8 +1,19 @@
-def backwards(text):
-    if len(text) <= 1:
-        return text
+#def backwards(text):
+#    if len(text) <= 1:
+#        return text
+#
+#    return backwards(text[1:]) + text[0]
+#run = backwards(text="hoppsan")
+#print(run)
 
-    return backwards(text[1:]) + text[0]
-string = "ojsan"
-run = backwards(string)
+def caps(text):
+    result=[]
+    for c in text:
+        if c.isupper():
+            result.append(c)
+    result1=len(result)
+    return result1
+
+
+run =caps(text="MåNdAg")
 print(run)
