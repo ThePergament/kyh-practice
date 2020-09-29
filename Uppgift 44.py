ea0532a509a21a92092859b9db8acf9119937b3a
david <david.pergament@student.kyh.se>
2020-09-29 13:40:56 +0200

    44.1,44.2

---
+++ Uppgift 44.py
@@ -0,0 +1,18 @@
+#1. Bygg ett program som tar in användarens namn och ålder. Lagra resultatet i en tuppel (str, int). Skicka tuppeln till en funktion, som skriver ut:
+#Namn: <name>     Ålder: <age>
+#44.1
+#lol=(input("Ange name: "),int(input("Ange ålder: ")))
+#def kanin(potatismos):
+#    print(f"Namn: {potatismos[0]}\n"
+ #         f"Ålder: {potatismos[1]}")
+
+#kanin(lol)
+#44.2
+#numtupp= (1,2)
+#def switch(oldnumber):
+#    newnumber = (oldnumber[1],oldnumber[0])
+#    return newnumber
+
+#print(switch(numtupp))
+
+
