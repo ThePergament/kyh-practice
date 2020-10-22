679f6af5337b88d86eff823086edb565f3cc6fbb
david <david.pergament@student.kyh.se>
2020-10-22 10:58:33 +0200

    Uppgift 51.1,51.2,51.3

---
+++ Uppgift 50.py
@@ -0,0 +1,40 @@
+#1
+#name = input("Hi, what's your name?")
+#print(f"Hi, {name}")
+#2
+#num1=int(input("Please insert number 1:"))
+#num2=int(input("Please insert number 2:"))
+#result= num1 + num2
+#print ("The result is", result)
+#3
+#result=(2+3)*6
+#print (result)
+#4
+#vowels="aeiouy"
+#character=input("Enter a character:" )
+#if character in vowels:
+#    print (character,"is a vowel")
+#else:
+#    print(character, "is not a vowel")
+#5
+#age=int(input("How old are you? "))
+#teen =15
+#if age >12 and age <20:
+#    print ("You are a teen")
+#else:
+#    print("You are not a teen")
+#6
+#notes={
+#    "1":"George Washington",
+#    "2":"Thomas Jefferson",
+#    "5":"Abraham Lincon",
+#    "10":"Alexander Hamilton",
+#    "20":"Andrew Jackson",
+#    "50":"Ulysses S.Grant",
+#    "100":"Benjamin Franklin"
+#}
+#note=input("Please enter note:")
+#if note not in notes:
+#    print (f"I'm sorry,{note} is not a valid US dollar note")
+#else:
+#    print ("On this note is",notes[note])
